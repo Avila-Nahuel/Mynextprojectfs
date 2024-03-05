ff25579eb45f1ba14cd63d1ab655177a98a5e1c0
Avila-Nahuel <avilanahuel14@gmail.com>
2024-03-04 21:24:06 -0300

    AboutPageDesing-ContactPageDesing

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,11 +3,38 @@ import styles from './about.module.css'
 
 const AboutPage = () => {
     return(
-        <div>
-            <div className={styles.imgContainer}>
-            <Image src="/about.png" alt="" fill/>
+    <div className={styles.container}>
+        <div className={styles.textContainer}>
+            <h2 className={styles.subtitle}>About Agency</h2>
+            <h1 className={styles.title}>We create digital ideas that are bigger, bolder, braver and better.</h1>
+            <p className={styles.description}>We create digital ideas that are bigger, bolder, braver and better.
+                We believe in good ideas flexibility and precission we are word is 
+                Our Special Team best consulting & finance solution provider. Wide 
+                reange of web software development services.
+            </p>
+            <div className={styles.boxes}>
+                <div className={styles.box}>
+                    <h1>10 K+</h1>
+                    <p>Year of experience</p>
+                </div>
+                <div className={styles.box}>
+                    <h1>10 K+</h1>
+                    <p>Year of experience</p>
+                </div>
+                <div className={styles.box}>
+                    <h1>10 K+</h1>
+                    <p>Year of experience</p>
+                </div>
             </div>
         </div>
+            <div className={styles.imgContainer}>
+                <Image
+                src='/about.png'
+                alt="About img"
+                fill
+                className={styles.img}/>
+            </div>   
+    </div>
     )
 }
 
