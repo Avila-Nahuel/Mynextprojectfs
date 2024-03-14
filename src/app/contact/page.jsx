9f19660c1ec2ccefd129cbbe2d59cdd373f01205
Avila-Nahuel <avilanahuel14@gmail.com>
2024-03-14 17:11:34 -0300

    next.jsRederingExplained

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,19 +1,24 @@
+"use client";
+
 import styles from './contact.module.css'
 import Image from 'next/image'
 
 const ContactPage = () => {
-    return(
+
+    console.log('it worjs here!')
+
+     return(
         <div className={styles.container}>
             <div className={styles.imgContainer}>
-                <Image src="/contact.png" alt='' fill/>
+                <Image src="/contact.png" sizes='"(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"' priority alt='ad' fill/>
             </div>
             <div className={styles.formContainer}>
                 <form action="" className={styles.form}>
                     <input type="text" placeholder='Name and Surname'/>
                     <input type="text" placeholder='Email Adress'/>
                     <input type="text" placeholder='Phone Number (Optional)'/>
-                    <textarea name="" id="" cols="30" rows="10"></textarea>
-                    <button>Send</button>
+                    <textarea name="" id="" cols="30" rows="10" placeholder='Message'></textarea>
+                    <button onClick={()=>console.log("clicked")}>Send</button>
                 </form>
             </div>
         </div>
